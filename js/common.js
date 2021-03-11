bcd0bb0a816cf6be92e1f5dcf363eb0fa8588223
takeitkhan <takeitkhan@gmail.com>
2021-03-11 19:58:49 +0600

    Template initiated

---
+++ js/common.js
@@ -0,0 +1,14 @@
+//initialized
+var carousel = $("#carousel").featureCarousel({
+    autoPlay: 0,
+    trackerIndividual: false,
+    trackerSummation: false,
+    smallFeatureWidth: .8,
+    smallFeatureHeight: .8,
+    topPadding: 0,
+    sidePadding: 0,
+    smallFeatureOffset: 90,
+    carouselSpeed: 500,
+    leftButtonTag: '#carousel-left',
+    rightButtonTag: '#carousel-right',
+});
